2333da2296c16b7aa93e2cd0c2bd64c17319e41b
unknown <you@example.com>
2026-08-20 14:13:28 +0530

    feat: migrate to true push architecture by removing polling heartbeat

--- src/main.cpp
+++ src/main.cpp
@@ -973,22 +973,6 @@ void setup() {
     startupTime = millis();
 }
 
-
-void publish_heartbeat() {
-    if (!mqttConnected || !auctionDataLoaded) return;
-    String msgId = "HB_" + String(millis());
-    JsonDocument doc;
-    doc["Action"] = "HEARTBEAT";
-    doc["Message_ID"] = msgId;
-    doc["Device_ID"] = deviceId;
-    if (selectedAuctionId.length() > 0) {
-        doc["Current_Auction"] = selectedAuctionId;
-    }
-    char buffer[256];
-    size_t n = serializeJson(doc, buffer);
-    mqttClient.publish(AUCTION_REQ_TOPIC, buffer, n);
-}
-
 // ------------------ LOOP ------------------
 void loop() {
     // 3-Second Long Press requirement for Power Off
@@ -1034,12 +1018,7 @@ void loop() {
     } 
     handleNetwork();  
     
-    // Heartbeat every 5 seconds to trigger AWS DB Sync
-    static unsigned long lastHeartbeatTime = 0;
-    if (mqttConnected && (millis() - lastHeartbeatTime > 5000)) {
-        lastHeartbeatTime = millis();
-        publish_heartbeat();
-    }
+    
 
     updateInputs();      // Buttons + keypad + NFC
 
